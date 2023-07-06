67f2a5f39dc562ebffab52c77a30676c82f147a9
Jasil Shan <jasilshan786@gmail.com>
2023-07-06 16:59:25 +0530

    axios added

---
+++ client/Eco cleanse/src/axios/axios.js
@@ -0,0 +1,27 @@
+import axios from 'axios' ;
+
+const axiosInstance = (tokenName)=> {
+  const instance = axios.create({
+    baseURL: import.meta.env.SERVER_URL ,
+    timeout : 5000 ,
+    headers :{
+      'Content-Type' : 'application/json'
+    }
+  })
+  // instance request interceptor 
+  instance.interceptors.request.use((request)=>{
+    const token = localStorage.getItem(tokenName)
+    request.headers.Authorization = `Bearer ${token}`
+    return request 
+  })
+
+  // instance response interceptor
+  instance.interceptors.response.use( response => response ,
+    error => Promise.reject(error.response.data)
+    )
+
+    return instance
+
+}
+
+export default axiosInstance
