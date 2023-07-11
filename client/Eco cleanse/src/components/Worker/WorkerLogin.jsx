646d4c1f358801bc10180bc90ff486a59d398cd6
Jasil Shan <jasilshan786@gmail.com>
2023-07-11 11:09:05 +0530

    show password function added

--- client/Eco cleanse/src/components/Worker/WorkerLogin.jsx
+++ client/Eco cleanse/src/components/Worker/WorkerLogin.jsx
@@ -1,46 +1,69 @@
 import React, { useState } from 'react'
 import work from './assets/work.jpg'
-import { useNavigate, Link } from "react-router-dom";
+import { useFormik } from 'formik'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios';
+import { toast } from "react-toastify";
+import { AiFillEye,AiFillEyeInvisible } from "react-icons/ai";
+
+
+
 
 
 const WorkerLogin = () => {
-
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
     const [errMessage, setErrMessage] = useState("")
     const navigate = useNavigate()
+    const [showPassword, setShowPassword] = useState(false);
+    const handleTogglePassword = () => {
+        setShowPassword(!showPassword);
+    };
 
+    const formik = useFormik({
+        initialValues: {
+          email: '',
+          password: '',
+        },
+        onSubmit:async (values) => {
+            try {
+                console.log(email);
+                const { data } = await axios.post('/worker/login', {...values})
+                console.log(data);
+                if(data.blocked){
+                  return toast.error(data.message, {
+                        position: "top-center",
+        
+                    })
+                }
+                if (data.error) {
+                    toast.error(data.message, {
+                        position: "top-center",
+        
+                    })
+                } else {
+                    navigate("/home")
+                }
+            } catch (error) {
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log(email);
-        const { data } = await axios.post("/worker/login",{ email, password })
-        console.log(data);
-        if (data.err) {
-            setErrMessage(data.message)
-        } else {
-            navigate("/worker")
-        }
-    }
+                console.log(error);
+            }
+        
+        },
+      })
+
     return (
 
-        <section className="bg-black min-h-screen flex items-center justify-center">
+         <section className="bg-black min-h-screen flex items-center justify-center">
             <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
 
                 <div className="md:w-1/2 px-16">
-                    <h2 className='font-bold text-4xl text-[#002D74]'>Login</h2>
+          <h2 className='font-bold text-3xl text-[#002D74]'>Login</h2>
                     <p className='text-sm mt-4 text-[#002D74]'>Welcome Back Collector!</p>
 
-                    <form onSubmit={handleSubmit} className='flex flex-col gap-6'>
-                        <input className='p-2 mt-8 rounded-xl border' type="email" name="email" placeholder='Email' id="" onChange={(e) => setEmail(e.target.value)} />
+                    <form onSubmit={formik.handleSubmit} className='flex flex-col gap-6'>
+                        <input className='p-2 mt-8 rounded-xl border' onChange={formik.handleChange} type="email" name="email" placeholder='Email' id="email" required/>
                         <div className="relative">
-                            <input className='p-2  rounded-xl border w-full' type="password" name='password' placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
-                            <svg className="w-5 absolute top-1 right-3 translate-y-1/2 h-4 text-gray-500" ariaHidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 14">
-                                <g stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2">
-                                    <path d="M10 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
-                                    <path d="M10 13c4.97 0 9-2.686 9-6s-4.03-6-9-6-9 2.686-9 6 4.03 6 9 6Z" />
-                                </g>
-                            </svg>
+                        <input className='p-2  rounded-xl border w-full' onChange={formik.handleChange} type={showPassword ? 'text' : 'password'} name='password' placeholder='Password' required />
+                            <button type='button' onClick={handleTogglePassword}> {!showPassword ? <AiFillEye className="w-5 absolute top-1 right-3 translate-y-1/2 h-4"/> : <AiFillEyeInvisible className="w-5 absolute top-1 right-3 translate-y-1/2 h-4"/> } </button>
                         </div>
                         <button className='bg-[#002D74] rounded-xl py-2 mt-2 text-white hover:scale-105 duration-300' type="submit">Login</button>
                     </form>
@@ -49,7 +72,7 @@ const WorkerLogin = () => {
                 <div className="w-1/2 md:block hidden">
                     <img className=' rounded-2xl' src={work} alt="" />
                 </div>
-            </div>
+      </div>
         </section>
     )
 }
