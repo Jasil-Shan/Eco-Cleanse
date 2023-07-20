1fe9dc8588ab152e8d8e2c1aa4bde95052608ea8
Jasil Shan <jasilshan786@gmail.com>
2023-07-20 22:14:33 +0530

    auth user added

--- client/src/services/userApi.js
+++ client/src/services/userApi.js
@@ -5,4 +5,8 @@ export const userLogin = (values) =>{
     return axiosInstance("JwtToken").post('/user/login' , {...values})
   }
 
+  export const authUser = ()=>{
+    return axiosInstance("JwtToken").post('/user/auth' )
+
+  }
 
