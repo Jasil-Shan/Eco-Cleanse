ce4eac6d88cf3785d909aac1a778d98e0062d179
Jasil Shan <jasilshan786@gmail.com>
2023-07-23 20:20:55 +0530

    added userAuth

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import { generateOTP, login, signUp } from '../controller/authController.js'
+import { generateOTP, login, signUp, userAuth } from '../controller/authController.js'
 
 
 
 const router = express.Router()
 
-
+router.post('/auth', userAuth)
 router.post('/verify',generateOTP)
 router.post('/signup', signUp)
 router.post('/login', login)
