e97096a4ee61e5d352cb107a2e6a9208cafdc133
Jasil Shan <jasilshan786@gmail.com>
2023-07-24 22:10:49 +0530

    userBooking added

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,12 +1,20 @@
+import UserModel from "../model/userModel.js"
 
 
 
 
 export async function userBooking(req, res) {
     try {
+
+        const {eWaste,foodWaste,plasticWaste,others} = req.body
+        console.log(req.body,"shaGSHG");
+        const id = req.userId
+        const user = await UserModel.findById(id)
+        console.log("sdghdsg",user,'shgdhgsd');
+        user.garbageDetails.push({eWaste,foodWaste,plasticWaste,others})
         
     } catch (error) {
-        
+        console.log(error);
     }
 
 }
