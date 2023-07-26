746d1ed2fc7067504c990f72c8484a672ccdc859
Jasil Shan <jasilshan786@gmail.com>
2023-07-26 19:35:37 +0530

    bug fixed

--- client/src/components/User/Hero/HeroLeft.jsx
+++ client/src/components/User/Hero/HeroLeft.jsx
@@ -5,12 +5,12 @@ const HeroLeft = (props) => {
     const { user, recycle } = props;
   
     return (
-      <div className="hero mt-16">
+      <div className="hero mt-28">
         <div className="hero-content flex-col lg:flex-row">
           {user && <img src={user} className="max-w-xs rounded-lg shadow-2xl" />}
           {recycle && <img src={recycle} className="max-w-xs rounded-lg shadow-2xl" />}
           <div className="ml-10">
-            <h1 className="text-3xl font-bold">Box Office News!</h1>
+            <p className="text-4xl font-bold">Just a Click and Update Details </p>
             <p className="py-6">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
