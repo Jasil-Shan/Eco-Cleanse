1daf0e9c7215052610142cd45b79889820e04e5c
Jasil Shan <jasilshan786@gmail.com>
2023-07-28 19:25:02 +0530

    Page added

---
+++ client/src/components/Driver/DriverDash.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import SidebarEmp from "../SidebarEmployee/SidebarEmp";
+import ModalEmp from "../ModalEmployee/ModalEmp";
+  
+const DriverDash = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    setShowModal(true);
+  }, []);
+
+  return (
+    <>
+        <SidebarEmp />
+        <ModalEmp />
+    </>
+
+  );
+};
+
+
+export default DriverDash;
