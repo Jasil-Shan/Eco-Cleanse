9d4e30ace990c8980358e0ad966930627325eeda
Jasil Shan <jasilshan786@gmail.com>
2023-07-29 21:52:31 +0530

    Api Added

--- client/src/services/adminApi.js
+++ client/src/services/adminApi.js
@@ -11,7 +11,17 @@ export const authAdmin = ()=>{
   return axiosInstance("AdminJwtToken").post("/admin/auth")
 }
 
+
+
 export const getUsers = ()=>{
     return axiosInstance("AdminJwtToken").get('/admin/users')
+}
+
+export const getWorks = ()=>{
+  return axiosInstance("AdminJwtToken").get('/admin/works')
+}
+
+export const getWorkers = ()=>{
+  return axiosInstance("AdminJwtToken").get('/admin/workers')
 }
 
