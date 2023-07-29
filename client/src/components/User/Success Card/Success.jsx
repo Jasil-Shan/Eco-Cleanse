639fb210afd0b876ddd0dac4758d5262aa53ff6a
Jasil Shan <jasilshan786@gmail.com>
2023-07-29 21:40:45 +0530

    Component Added

---
+++ client/src/components/User/Success Card/Success.jsx
@@ -0,0 +1,24 @@
+import SuccessSvg from '../assets/success.svg'
+
+
+
+const Success = () => {
+    return (
+        <div className="min-h- py-6 flex flex-col justify-center items-center sm:py-12 ">
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure className="px-10 pt-10">
+                <object data={SuccessSvg} type="image/svg+xml" className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+                <p className="card-title font-extrabold text-2xl">Request Success</p>
+                <p className='font-mono'>Our Team will reach You soon</p>
+                <div className="card-actions mt-6">
+                    {/* <button className="btn-sm btn-accent">Buy Now</button> */}
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default Success
