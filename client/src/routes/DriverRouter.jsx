8011597c2adf715a071edb1ee2e958e69259c8c0
Jasil Shan <jasilshan786@gmail.com>
2023-07-29 21:52:14 +0530

    Routes added

--- client/src/routes/DriverRouter.jsx
+++ client/src/routes/DriverRouter.jsx
@@ -1,5 +1,7 @@
+import DriverDashPage from "../pages/Driver/DriverDashPage";
 import DriverLoginPage from "../pages/Driver/DriverLoginPage"
 import { Route, Routes } from "react-router-dom";
+import DriverWelcomePage from "../pages/Driver/DriverWelcomePage";
 
 
 
@@ -9,7 +11,8 @@ function DriverRouter(){
     return(
         <Routes>
             <Route path="login" element={<DriverLoginPage/>}/>
-
+            <Route path="dashboard" element={<DriverDashPage/>}/>
+            <Route path="welcome" element={<DriverWelcomePage/>}/>
         </Routes>
     )
 }
