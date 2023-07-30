4746873fc968030b75e625581087869cfc58fcb9
Jasil Shan <jasilshan786@gmail.com>
2023-07-30 20:43:12 +0530

    route Added

--- client/src/routes/DriverRouter.jsx
+++ client/src/routes/DriverRouter.jsx
@@ -2,17 +2,25 @@ import DriverDashPage from "../pages/Driver/DriverDashPage";
 import DriverLoginPage from "../pages/Driver/DriverLoginPage"
 import { Route, Routes } from "react-router-dom";
 import DriverWelcomePage from "../pages/Driver/DriverWelcomePage";
+import PrivateRoutes from "../utils/PrivateRoutes";
 
 
 
 
 
-function DriverRouter(){
-    return(
+function DriverRouter() {
+    return (
         <Routes>
-            <Route path="login" element={<DriverLoginPage/>}/>
-            <Route path="dashboard" element={<DriverDashPage/>}/>
-            <Route path="welcome" element={<DriverWelcomePage/>}/>
+
+            <Route element={<PrivateRoutes role={'driver'} route={'/driver/login'} />}>
+
+                <Route path="dashboard" element={<DriverDashPage />} />
+                <Route path="welcome" element={<DriverWelcomePage />} />
+
+            </Route>
+
+            <Route path="login" element={<DriverLoginPage />} />
+
         </Routes>
     )
 }
