b24c546d8c30a4bcbd448270e912a59c6ba37966
Jasil Shan <jasilshan786@gmail.com>
2023-07-30 23:57:20 +0530

    added sidebar

---
+++ client/src/utils/EmployeeSidebar.jsx
@@ -0,0 +1,39 @@
+import { AiFillHome } from "react-icons/ai";
+
+
+
+
+export const driverSidebar = [
+    {
+        icon: <AiFillHome size={22}/>,
+        link: "/admin/dashboard"
+     },
+     {
+        icon: <AiFillHome size={22}/>,
+        link: "/admin/dashboard"
+     },
+     {
+        icon: <AiFillHome size={22}/>,
+        link: "/admin/dashboard"
+     },
+        
+]
+
+export const workerSidebar = [
+    {
+        icon: 'Dashboard',
+        link: "/admin/dashboard"
+     },
+     {
+        icon: 'Dashboard',
+        link: "/admin/dashboard"
+     },
+     {
+        icon: 'Dashboard',
+        link: "/admin/dashboard"
+     },
+        
+]
+
+
+
