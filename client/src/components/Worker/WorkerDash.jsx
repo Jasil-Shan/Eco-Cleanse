aa35f11dd932668d0364498bd96021bbee34e6cc
Jasil Shan <jasilshan786@gmail.com>
2023-08-01 03:21:47 +0530

    conditional rendering added

--- client/src/components/Worker/WorkerDash.jsx
+++ client/src/components/Worker/WorkerDash.jsx
@@ -1,20 +1,46 @@
 import React, { useEffect, useState } from "react";
 import SidebarEmp from "../SidebarEmployee/SidebarEmp";
 import ModalEmp from "../ModalEmployee/ModalEmp";
-  
+import ProfileCard from "../Employees/ProfileCard/ProfileCard";
+import { authWorker } from "../../services/workerApi";
+import Status from "../Employees/Status/Status";
+
 const WorkerDash = () => {
   const [showModal, setShowModal] = useState(false);
+  const [worker, setWorker] = useState()
 
   useEffect(() => {
     setShowModal(true);
+    try {
+      (
+        async function () {
+          const { data } = await authWorker()
+          if (data.status) {
+            setWorker(data.worker)
+            console.log(worker);
+          }
+        })()
+    } catch (error) {
+      console.log(error);
+    }
   }, []);
+
 
   return (
     <>
-        <SidebarEmp />
-        <ModalEmp />
-    </>
+      <SidebarEmp />
+      
+      {
+      worker && worker.location ?
+        <div className="h-screen w-screen flex justify-evenly space-x-12 align-middle items-center mt-0">
+          <Status status = {worker.status} />
+          <ProfileCard profile={worker} />
+        </div>
+        :
+        <ModalEmp role = {'worker'}/>
 
+      }
+    </>
   );
 };
 
