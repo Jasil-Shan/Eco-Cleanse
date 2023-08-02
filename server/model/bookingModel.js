89f7eb982a7c64f6178c781f62678521940167bf
Jasil Shan <jasilshan786@gmail.com>
2023-08-02 19:49:19 +0530

    model created

---
+++ server/model/bookingModel.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose"
+
+const bookingSchema = new mongoose.Schema(
+    {
+      user :  {
+        type : mongoose.Schema.Types.ObjectId ,
+        ref : 'Users' ,
+        required : true
+      },
+      worker :  {
+        type : mongoose.Schema.Types.ObjectId ,
+        ref : 'workers' ,
+      },
+      driver :  {
+        type : mongoose.Schema.Types.ObjectId ,
+        ref : 'drivers' ,
+      },
+      order_id :{
+        type: String,
+      },
+      garbage:{    
+        eWaste: {
+          type: String,
+          required: true,
+        },
+        foodWaste: {
+          type: String,
+          required: true,
+        },
+        plasticWaste: {
+          type: String,
+          required: true,
+        },
+        others: {
+          type: String,
+          required: true,
+        }},
+
+      paymentMethod: {
+        type: String,
+        required:true
+      },
+      assigned:{
+        type:Boolean,
+        default:false
+      }
+    },
+    { timestamps: true }
+  );
+
+  const BookingModel = mongoose.model("bookings", bookingSchema)
+
+export default BookingModel
