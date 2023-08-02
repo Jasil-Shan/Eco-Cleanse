26692cdc7a42247253e7649004b5c868f6a57bea
Jasil Shan <jasilshan786@gmail.com>
2023-08-02 19:54:30 +0530

    api added

--- client/src/services/driverApi.js
+++ client/src/services/driverApi.js
@@ -9,6 +9,20 @@ export const authDriver = () =>{
 
 
 
-export const driverLocation = (location) =>{
+export const getLocation = (location,role) =>{
+  if(role == 'driver'){
     return axiosInstance("DriverJwtkey").post('/driver/UpdateLocation' , {location})
+  }else{
+    return axiosInstance("WorkerJwtkey").post('/worker/UpdateLocation' , {location})
+
   }
+  }
+
+  export const updateStatus = (location,role,status) =>{
+    if(role == 'driver'){
+      return axiosInstance("DriverJwtkey").patch('/driver/statusUpdate',{location,status})
+    }else{
+      return axiosInstance("WorkerJwtkey").patch('/worker/statusUpdate',{location,status})
+
+    }
+    }
