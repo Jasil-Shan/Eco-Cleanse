27c598f0b0dcd8690754722e6d12af8f338edfe5
Jasil Shan <jasilshan786@gmail.com>
2023-08-02 13:31:05 +0530

    logout removed

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -26,12 +26,7 @@ const Header = () => {
         {/* <div className="flex">
             <button>Logout</button>
         </div> */}
-        <p className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-          onClick={() => {
-            localStorage.removeItem('AdminJwtToken');
-            navigate('/admin/login')
-          }}
-        >Logout</p>
+        
       </div>
     </header>
   );
