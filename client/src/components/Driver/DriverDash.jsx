6b5fb30d821f4ff14177cd2d978aa4a71612e2f8
Jasil Shan <jasilshan786@gmail.com>
2023-08-02 19:43:37 +0530

    useEffect added

--- client/src/components/Driver/DriverDash.jsx
+++ client/src/components/Driver/DriverDash.jsx
@@ -1,23 +1,45 @@
 import React, { useEffect, useState } from "react";
 import SidebarEmp from "../SidebarEmployee/SidebarEmp";
 import ModalEmp from "../ModalEmployee/ModalEmp";
-  
+import { authDriver } from "../../services/driverApi";
+import ProfileCard from "../Employees/ProfileCard/ProfileCard";
+import Status from "../Employees/Status/Status";
+
 const DriverDash = () => {
   const [showModal, setShowModal] = useState(false);
-
+  const [driver ,setDriver] = useState()
   useEffect(() => {
+    try {
+      (
+        async function () {
+          const { data } = await authDriver()
+          console.log(data);
+          if (data.status) {
+            setDriver(data.driver)
+          }
+        })()
+    } catch (error) {
+      console.log(error);
+    }
     setShowModal(true);
   }, []);
 
   return (
     <>
-        <SidebarEmp />
-        <ModalEmp />
-    
-    </>
+      <SidebarEmp />
+      {
+        driver && driver.location ?
+          <div className="h-screen w-screen flex justify-evenly space-x-12 align-middle items-center mt-0">
+          <Status status = {driver.status} role = {driver.role}/>
+            <ProfileCard profile={driver} />
+          </div>
+          :
+          <ModalEmp role={'driver'} />
 
+      }
+    </>
   );
-}; 
+};
 
 
 export default DriverDash;
