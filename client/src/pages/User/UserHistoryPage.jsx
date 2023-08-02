64f1c90ea78a83feeb034c022da8ddddb07ce177
Jasil Shan <jasilshan786@gmail.com>
2023-08-03 00:23:01 +0530

    page added

---
+++ client/src/pages/User/UserHistoryPage.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../../components/User/Navbar/Navbar"
+import Table from "../../components/User/Table/Table"
+
+
+
+const UserHistoryPage = ()=>{
+
+    return(
+        <>
+        <Navbar />
+        <Table />
+        </>
+    )
+}
+
+
+export default UserHistoryPage
