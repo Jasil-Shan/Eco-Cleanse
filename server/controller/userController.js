bfc80589c516893a4af24976b263aa2d54c21106
Jasil Shan <jasilshan786@gmail.com>
2023-08-03 22:53:58 +0530

    order id created

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,20 +1,37 @@
+import orderId from "order-id";
 import BookingModel from "../model/bookingModel.js";
-import UserModel from "../model/userModel.js"
 
 
 export async function userBooking(req, res) {
   try {
     const { garbage, payment } = req.body;
     const id = req.userId;
+    const order_id = orderId.generate();
+
 
     await BookingModel.create({
       garbage: garbage,
       paymentMethod: payment,
-      user: id
+      user: id,
+      order_id
     })
     res.json({ success: true, message: "Added successfully" });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: "Failed" });
   }
+}
+
+
+export async function getBookings(req, res) {
+
+  try {
+
+    const bookings = await BookingModel.find({})
+    res.json({ status: true,bookings, message: "success" })
+
+  } catch (error) {
+    console.log(error);
+  }
+
 }
