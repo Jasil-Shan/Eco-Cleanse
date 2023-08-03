4b129b31c59f6c42e7d2c39d855b63ac3b901342
Jasil Shan <jasilshan786@gmail.com>
2023-08-03 23:30:29 +0530

    json limit increased

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use(
 
 dbConnect()
 
-app.use(express.json())
+app.use(express.json({limit:"50mb"})) 
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.resolve()+"/public"))
