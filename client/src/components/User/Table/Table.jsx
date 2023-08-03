711103934b50a016ca9fa22157460753fd27ccec
Jasil Shan <jasilshan786@gmail.com>
2023-08-03 22:53:08 +0530

    component added

--- client/src/components/User/Table/Table.jsx
+++ client/src/components/User/Table/Table.jsx
@@ -1,54 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getBookings } from '../../../services/userApi';
 
 const Table = () => {
+  const [bookings, setBookings] = useState([])
+  useEffect(() => {
+    try {
+      (
+        async function () {
+          const { data } = await getBookings()
+          if (data.status) {
+            setBookings(data.bookings)
+            console.log(bookings);
+          }
+        })()
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+
   return (
+
     <>
-    <section className="container mx-auto p- mt-16 font-mono">
-    <h1 className='font-extrabold text-3xl ml-3 mb-8 '>REQUEST HISTORY</h1>
-      <div className="w-full mb-8 overflow-hidden rounded-lg shadow-2xl">
-        <div className="w-full overflow-x-auto">
-          <table className="w-full">
-            <thead>
-              <tr className="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
-                <th className="px-4 py-3">Order Id</th>
-                <th className="px-4 py-3">Date</th>
-                <th className="px-4 py-3">Payment</th>
-                <th className="px-4 py-3">Status</th>
-                <th className="px-4 py-3">Date</th>
-              </tr>
-            </thead>
-            <tbody className="bg-white">
-              <tr className="text-gray-700">
-                <td className="px-4 py-3 border">
-                  <div className="flex items-center text-sm">
-                    <div className="relative w-8 h-8 mr-3 rounded-full md:block">
-                      <img
-                        className="object-cover w-full h-full rounded-full"
-                        src="https://images.pexels.com/photos/5212324/pexels-photo-5212324.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
-                        alt=""
-                        loading="lazy"
-                      />
-                      <div className="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                    </div>
-                    <div>
-                      <p className="font-semibold text-black">Sufyan</p>
-                      <p className="text-xs text-gray-600">Developer</p>
-                    </div>
-                  </div>
-                </td>
-                <td className="px-4 py-3 border text-ms font-semibold">22</td>
-                <td className="px-4 py-3 border text-xs">
-                  <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm">Acceptable</span>
-                </td>
-                <td className="px-4 py-3 border text-sm">6/4/2000</td>
-                <td className="px-4 py-3 border text-sm">6/4/2000</td>
-              </tr>
-              {/* Add more rows here */}
-            </tbody>
-          </table>
+   
+      <section className="container mx-auto p- mt-16 font-mono">
+        <h1 className='font-extrabold text-3xl ml-3 mb-8 '>REQUEST HISTORY</h1>
+        <div className="w-full mb-8 overflow-hidden rounded-lg shadow-2xl">
+          <div className="w-full overflow-x-auto">
+            <table className="w-full">
+              <thead>
+                <tr className="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
+                  <th className="px-4 py-3">Order Id</th>
+                  <th className="px-4 py-3">Date</th>
+                  <th className="px-4 py-3">Status</th>
+                  <th className="px-4 py-3">Payment</th>
+                </tr>
+              </thead>
+              <tbody className="bg-white">
+                {
+                  bookings.map((item,index)=>{ 
+                    return <tr key={index} className="text-gray-700">
+                    <td className="px-4 py-3 border"> 
+                      <div className="flex items-center text-sm">
+                        <div>
+                          <p className="font-semibold text-black">{item.order_id}</p>
+                          <p className="text-xs text-gray-600">Developer</p>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="px-4 py-3 border text-ms font-semibold">{item.createdAt}</td>
+                    <td className="px-4 py-3 border text-xs">
+                      <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm">{item.status}</span>
+                    </td>
+                    <td className="px-4 py-3 border text-sm">{item.paymentMethod}</td>
+                  </tr>
+                   })
+                }
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+
     </>
   );
 };
