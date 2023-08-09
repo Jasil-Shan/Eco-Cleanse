c3e4e93f762a87bf64ce83065a68bd34af70c01f
Jasil Shan <jasilshan786@gmail.com>
2023-08-09 21:17:17 +0530

    helper added

---
+++ client/src/helpers/calculateDistance.js
@@ -0,0 +1,19 @@
+
+
+export function calculateDistance(lat1, lon1, lat2, lon2) {
+    const earthRadius = 6371;
+    const dLat = ((lat2 - lat1) * Math.PI) / 180;
+    const dLon = ((lon2 - lon1) * Math.PI) / 180;
+
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos((lat1 * Math.PI) / 180) *
+        Math.cos((lat2 * Math.PI) / 180) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    const distance = earthRadius * c;
+    return distance;
+  }
