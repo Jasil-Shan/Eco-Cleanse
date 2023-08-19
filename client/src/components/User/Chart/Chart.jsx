30b2d2af4b87bebe70920a156eaf13a8bcbd3bfd
Jasil Shan <jasilshan786@gmail.com>
2023-08-19 17:34:13 +0530

    chart added

---
+++ client/src/components/User/Chart/Chart.jsx
@@ -0,0 +1,192 @@
+import React, { useState } from 'react';
+import ReactApexChart from 'react-apexcharts';
+import Navbar from '../Navbar/Navbar';
+import { useLocation } from 'react-router-dom';
+import { userOnlinePay } from '../../../services/userApi';
+
+const Chart = () => {
+  const [selectedOption, setSelectedOption] = useState('Cash')
+
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+  };
+  const getLocation = useLocation()
+  const { values } = getLocation?.state
+
+  const valuesArray = [
+    values.eWaste,
+    values.plasticWaste,
+    values.foodWaste,
+    values.others,
+  ];
+
+ const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+
+      if (selectedOption == 'Online') {
+
+        const { data: { order } } = await userOnlinePay()
+        var options = {
+          key: "rzp_test_X2EWEu9JQG1E2R",
+          amount: "250000",
+          currency: "INR",
+          name: "Eco Cleanse",
+          description: "Service Charge",
+          image: "https://example.com/your_logo",
+          order_id: order.id,
+          handler: async (response) => {
+            try {
+              await payment(response)
+              console.log(response);
+            } catch (error) {
+              console.error(error);
+            }
+          },
+          prefill: {
+            name: "Gaurav Kumar",
+            email: "gaurav.kumar@example.com",
+            contact: "9000090000"
+          },
+          notes: {
+            address: "Eco Cleanse Corporate Office"
+          },
+          theme: {
+            color: "#3399cc"
+          }
+        };
+        const razor = new window.Razorpay(options)
+        razor.open();
+
+      } else {
+
+        const { data } = await userBooking({ payment: selectedOption }, values);
+
+        if (data.success) {
+          setOrder(data.order_id)
+          setSuccess(true)
+          toast.success(data.message, {
+            position: "top-center",
+          });
+        } else {
+          toast.error(data.message, {
+            position: "top-center",
+          });
+        }
+      }
+
+      const payment = async (response) => {
+        try {
+          const { data } = await verifyPayment(response)
+          if (data.success) {
+            const { data } = await userBooking({ payment: selectedOption }, values);
+            setSuccess(true)
+            toast.success(data.message, {
+              position: "top-center",
+            });
+          } else {
+            toast.error(data.message, {
+              position: "top-center",
+            });
+          }
+
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const series = [...valuesArray];
+
+  const options = {
+    chart: {
+      height: 390,
+      type: 'radialBar',
+    },
+    plotOptions: {
+      radialBar: {
+        offsetY: 0,
+        startAngle: 0,
+        endAngle: 270,
+        hollow: {
+          margin: 5,
+          size: '30%',
+          background: 'transparent',
+          image: undefined,
+        },
+        dataLabels: {
+          name: {
+            show: true,
+          },
+          value: {
+            show: true,
+          },
+        },
+      },
+    },
+    colors: ['#e63946', '#1d3557', '#38b000', '#99582a'],
+    labels: ['Plastic Waste', 'E waste', 'Food Waste', 'Others'],
+    legend: {
+      show: true,
+      floating: true,
+      fontSize: '16px',
+      position: 'left',
+      offsetX: 160,
+      offsetY: 15,
+      labels: {
+        useSeriesColors: true,
+      },
+      markers: {
+        size: 0,
+      },
+      formatter: function (seriesName, opts) {
+        return seriesName + ':  ' + opts.w.globals.series[opts.seriesIndex] + ' %';
+      },
+      itemMargin: {
+        vertical: 3,
+      },
+    },
+    responsive: [
+      {
+        breakpoint: 480,
+        options: {
+          legend: {
+            show: false,
+          },
+        },
+      },
+    ],
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div id="chart" className='flex flex-col'>
+        <ReactApexChart options={options} series={series} type="radialBar" height={390} />
+        <div className="self-center flex gap-8 mb-8">
+          <div
+            className={`card border-2 drop-shadow-lg p-6 bg-white ${selectedOption === 'Cash' ? ' border-blue-700' : ''
+              }`}
+            onClick={() => handleOptionSelect('Cash')}
+          >
+            Cash
+          </div>
+          <div
+            className={`card border-2 drop-shadow-lg p-6 bg-white ${selectedOption === 'Online' ? 'border-blue-700' : ''
+              }`}
+            onClick={() => handleOptionSelect('Online')}
+          >
+            Online
+          </div>
+        </div>
+        <button className='btn btn-sm drop-shadow-lg w-fit self-center btn-success text-white'>Confirm</button>
+      </div>
+    </>
+  );
+}
+
+export default Chart
