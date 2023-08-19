3eed32cf13ca81ed231d6f0b60fcd2f633b042ef
Jasil Shan <jasilshan786@gmail.com>
2023-08-19 22:43:53 +0530

    new component added

---
+++ client/src/components/Employees/Tasks/TaskAlert/TaskAlert.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { FcBusinessman, FcHome, FcPhoneAndroid } from 'react-icons/fc';
+import { toast } from 'react-toastify';
+import { TaskAccept } from '../../../../services/driverApi';
+
+const TaskAlert = ({ task,role }) => {
+
+  const [isOpen, setIsOpen] = useState(true);
+  const taskId = task._id
+
+  const toggleModal = () => {
+    setIsOpen(false);
+  };
+
+  const handleSubmit = async ()=>{
+    const {data} = await TaskAccept(taskId ,role)
+    if(data.success){
+        toggleModal()
+        toast.success(data.message, {
+            position: "top-center"
+        })
+    }
+  }
+
+  return (
+    <>
+      {isOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75">
+          <div className="bg-white text-center px-8 rounded-lg shadow-xl mx-auto max-w-md">
+            <div className="card-body p-6">
+              <span className="antialiased tracking-wider text-xl font-bold">Task</span>
+              <div className="flex justify-evenly">
+                <div className="flex flex-col border-t-4 border-green-400 card shadow-xl p-6">
+                  <h1 className="uppercase font-bold tracking-wider mb-2 text-center">Customer</h1>
+                  <span className="inline-flex mt-3">
+                    <FcBusinessman size={26} />
+                    <p className="uppercase font-medium ml-8">{task?.user?.name}</p>
+                  </span>
+                  <span className="inline-flex mt-3 ">
+                    <FcHome size={26} />
+                    <p className="uppercase font-medium text ml-8">{task?.user?.address}</p>
+                  </span>
+                  <span className="inline-flex mt-3 ">
+                    <FcPhoneAndroid size={26} />
+                    <p className="uppercase font-medium ml-8">{task?.user?.mobile}</p>
+                  </span>
+                </div>
+              </div>
+              <div className="mt-4 flex justify-center space-x-4">
+                <button className="btn btn-sm bg-red-500 text-white" onClick={toggleModal}>
+                  Deny
+                </button>
+                <button className="btn btn-sm btn-primary" onClick={handleSubmit}>
+                  Accept
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default TaskAlert;
--- client/src/components/Employees/Tasks/Tasks.jsx
+++ client/src/components/Employees/Tasks/Tasks.jsx
@@ -3,18 +3,27 @@ import { getTasks } from "../../../services/driverApi"
 import { FaUserLarge } from "react-icons/fa6";
 import { FcBusinessman, FcHome, FcPhoneAndroid, FcReadingEbook } from "react-icons/fc";
 import { useNavigate } from "react-router-dom";
-import TaskModal from "./TaskModal/TaskModal";
+import TaskModal from "../../Worker/TaskModal/TaskModal";
+import TaskAlert from "./TaskAlert/TaskAlert";
+import { useSelector } from "react-redux";
 
 const Tasks = (props) => {
 
     const [task, setTask] = useState({})
     const TaskId = props.id
+    const role = props.role
     const navigate = useNavigate()
+    let profile
+    if (role == 'worker') {
+        profile = useSelector((state) => state.worker)
+    } else {
+        profile = useSelector((state) => state.driver)
+    }
 
     useEffect(() => {
         try {
             (async function () {
-                const { data } = await getTasks(TaskId)
+                const { data } = await getTasks(TaskId,role)
                 if (data.status) {
                     setTask(data.task)
                 }
@@ -24,36 +33,35 @@ const Tasks = (props) => {
             console.log(error);
         }
     }, [])
-
+    console.log(profile);
     return (
         <>
-            {
-                task?.user &&
+            {profile.assigned ? 
                 <div className="mt-12 ">
                     <div className="card w-auto h-auto lg:card-side mt-2 ml-4 bg-base-100 shadow-xl">
                         {/* <figure><img className="w-72 h-full" src="https://source.unsplash.com/rEn-AdBr3Ig" alt="Album" /></figure> */}
                         <div className="card-body  ">
                             <span className="antialiased tracking-wider text-xl font-bold">Task</span>
-                            <div className="flex  justify-evenly">
+                            <div className="flex justify-evenly">
                                 <div className=" flex flex-col  border-t-4 border-green-400 card shadow-xl w-fit p-6">
                                     <h1 className="uppercase font-bold tracking-wider mb-2 text-center">Customer</h1>
-                                    <span className="inline-flex mt-3"><FcBusinessman size={26} /><p className="uppercase font-medium   ml-8">{task.user.name}</p></span>
-                                    <span className="inline-flex mt-3 "><FcHome size={26} /><p className="uppercase font-medium text ml-8">{task.user.address}</p></span>
-                                    <span className="inline-flex mt-3 "><FcPhoneAndroid size={26} /><p className="uppercase font-medium  ml-8">{task.user.mobile}</p></span>
+                                    <span className="inline-flex mt-3"><FcBusinessman size={26} /><p className="uppercase font-medium   ml-8">{task?.user?.name}</p></span>
+                                    <span className="inline-flex mt-3 "><FcHome size={26} /><p className="uppercase font-medium text ml-8">{task?.user?.address}</p></span>
+                                    <span className="inline-flex mt-3 "><FcPhoneAndroid size={26} /><p className="uppercase font-medium  ml-8">{task?.user?.mobile}</p></span>
                                 </div>
                                 {(props.role == 'driver') ?
                                     <div className=" flex flex-col  border-t-4 border-green-400 card shadow-xl w-fit p-6">
                                         <h1 className="uppercase font-bold text-center tracking-wider mb-2 subpixel-antialiased">Worker</h1>
-                                        <span className="inline-flex mt-3"><FcReadingEbook size={26} /><p className="uppercase font-medium   ml-8">{task.worker.name}</p></span>
-                                        <span className="inline-flex mt-3  "><FcHome size={26} /><p className="uppercase  text-ellipsis overflow-hidden whitespace-nowrap w-28 hover:w-fit font-medium text ml-8">{task.worker.place}</p></span>
-                                        <span className="inline-flex mt-3 "><FcPhoneAndroid size={26} /><p className="uppercase font-medium  ml-8">{task.worker.mobile}</p></span>
+                                        <span className="inline-flex mt-3"><FcReadingEbook size={26} /><p className="uppercase font-medium   ml-8">{task?.worker?.name}</p></span>
+                                        <span className="inline-flex mt-3  "><FcHome size={26} /><p className="uppercase  text-ellipsis overflow-hidden whitespace-nowrap w-28 hover:w-fit font-medium text ml-8">{task?.worker?.place}</p></span>
+                                        <span className="inline-flex mt-3 "><FcPhoneAndroid size={26} /><p className="uppercase font-medium  ml-8">{task?.worker?.mobile}</p></span>
                                     </div>
                                     :
                                     <div className=" flex flex-col  border-t-4 border-green-400 card shadow-xl w-fit p-6">
                                         <h1 className="uppercase font-bold tracking-wider mb-2 text-center">Driver</h1>
-                                        <span className="inline-flex mt-3"><FcReadingEbook size={26} /><p className="uppercase font-medium   ml-8">{task.driver.name}</p></span>
-                                        <span className="inline-flex mt-3  "><FcHome size={26} /><p className="uppercase  text-ellipsis overflow-hidden whitespace-nowrap w-28 hover:w-fit font-medium text ml-8">{task.driver.place}</p></span>
-                                        <span className="inline-flex mt-3 "><FcPhoneAndroid size={26} /><p className="uppercase font-medium  ml-8">{task.driver.mobile}</p></span>
+                                        <span className="inline-flex mt-3"><FcReadingEbook size={26} /><p className="uppercase font-medium   ml-8">{task?.driver?.name}</p></span>
+                                        <span className="inline-flex mt-3  "><FcHome size={26} /><p className="uppercase  text-ellipsis overflow-hidden whitespace-nowrap w-28 hover:w-fit font-medium text ml-8">{task?.driver?.place}</p></span>
+                                        <span className="inline-flex mt-3 "><FcPhoneAndroid size={26} /><p className="uppercase font-medium  ml-8">{task?.driver?.mobile}</p></span>
                                     </div>
                                 }
                             </div>
@@ -63,6 +71,8 @@ const Tasks = (props) => {
                         </div>
                     </div>
                 </div>
+                :
+                <TaskAlert task ={task} role = {role}/>
             }
         </>
     )
