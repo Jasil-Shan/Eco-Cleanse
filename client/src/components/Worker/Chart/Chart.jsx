ce752350ad3c0f849dc93d55a47320d78de4ab2a
Jasil Shan <jasilshan786@gmail.com>
2023-08-21 20:21:32 +0530

    component added

---
+++ client/src/components/Worker/Chart/Chart.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactApexChart from 'react-apexcharts';
+
+const MixedPie = ()=> {
+
+  const series = []
+  const options = {
+    chart: {
+      height: 350,
+      type: 'radialBar',
+    },
+    plotOptions: {
+      radialBar: {
+        dataLabels: {
+          name: {
+            fontSize: '22px',
+          },
+          value: {
+            fontSize: '16px',
+          },
+          total: {
+            show: true,
+            label: 'Total',
+            formatter: function (w) {
+              return 2
+            },
+          },
+        },
+      },
+    },
+    labels: ['Drivers', 'Workers'],
+  };
+
+  return (
+    <div id="chart" className=' card shadow-md p-5 '>
+      <ReactApexChart options={options} series={series} type="radialBar" height={350} />
+    </div>
+  );
+}
+
+export default MixedPie
