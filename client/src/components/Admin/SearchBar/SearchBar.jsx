4a1619001716ccd24a101e64dd316c27a74de32e
Jasil Shan <jasilshan786@gmail.com>
2023-08-23 21:15:43 +0530

    bug fix

--- client/src/components/Admin/SearchBar/SearchBar.jsx
+++ client/src/components/Admin/SearchBar/SearchBar.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const SearchBar = ({setSearch}) => {
   return (
-    <div className='max-w-md mx-auto'>
-      <div className="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
+    <div className='max-w-md mt-8 mx-auto'>
+      <div className="relative flex shadow-xl p-4 items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
         <div className="grid place-items-center h-full w-12 text-gray-300">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
@@ -14,7 +14,7 @@ const SearchBar = ({setSearch}) => {
           type="text"
           id="search"
           onChange={({currentTarget:input})=>setSearch(input.value)}
-          placeholder="Search something.."
+          placeholder="Search "
         />
       </div>
     </div>
