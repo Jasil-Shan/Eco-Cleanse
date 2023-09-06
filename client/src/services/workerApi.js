c6cc2175739a1cb7c2fbde320c661fee9e8b0ba7
Jasil Shan <jasilshan786@gmail.com>
2023-09-06 11:28:14 +0530

    route added

--- client/src/services/workerApi.js
+++ client/src/services/workerApi.js
@@ -6,6 +6,6 @@ export const authWorker = () =>{
     return axiosInstance("WorkerJwtkey").post('/worker/auth')
 }
 
-export const taskComplete = (garbageDetails,taskId) =>{
-    return axiosInstance("WorkerJwtkey").post('/worker/taskComplete', { garbageDetails, taskId })
+export const taskComplete = (garbageDetails,taskId,location) =>{
+    return axiosInstance("WorkerJwtkey").post('/worker/taskComplete', { garbageDetails, taskId,location })
 }
