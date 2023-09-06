cb7ca34112c48f9ee31a67dfcc3a86d0a52910c1
Jasil Shan <jasilshan786@gmail.com>
2023-09-06 11:22:33 +0530

    ui fix

--- client/src/components/User/Success Card/Success.jsx
+++ client/src/components/User/Success Card/Success.jsx
@@ -5,7 +5,7 @@ import SuccessSvg from '../assets/success.svg'
 const Success = (props) => {
     const id = props.orderId
     return (
-        <div className="min-h- py-6 flex flex-col justify-center items-center sm:py-12 ">
+        <div className="min-h- py-6 flex flex-col justify-center mt-16 items-center sm:py-12 ">
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
                 <object data={SuccessSvg} type="image/svg+xml" className="rounded-xl" />
