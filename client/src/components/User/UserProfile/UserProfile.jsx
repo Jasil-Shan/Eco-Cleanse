df2c06c0c75470c492d9551b26c7e1186345b463
Jasil Shan <jasilshan786@gmail.com>
2023-09-06 11:23:13 +0530

    bug fixed

--- client/src/components/User/UserProfile/UserProfile.jsx
+++ client/src/components/User/UserProfile/UserProfile.jsx
@@ -10,16 +10,15 @@ import { UserProfileUpdate } from "../../../services/userApi";
 const UserProfile = () => {
   const [refresh, setRefresh] = useState()
   const [editMode, setEditMode] = useState(false)
-  const users = useSelector((state) => state.user)
+  const user = useSelector((state) => state.user)
 
   const formik = useFormik({
     initialValues: {
-      name: '',
-      mobile: '',
-    },
+      name: user.name,
+      mobile: user.mobile 
+       },
     onSubmit: async (values) => {
       try {
-
         const { data } = await UserProfileUpdate(values)
         if (data.success) {
           setRefresh(!refresh)
@@ -77,18 +76,18 @@ const UserProfile = () => {
                       />
                     </div>
                     <div className="text-center mt-12">
-                      <h3 className="text-4xl font-semibold leading-normal text-blueGray-700 mb-2">{users.name}</h3>
+                      <h3 className="text-4xl font-semibold leading-normal text-blueGray-700 mb-2">{user.name}</h3>
                       <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
                         <i className="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
-                        {users.address}
+                        {user.address}
                       </div>
                       <div className="flex items-center justify-center mb-4 mt-5">
                         <BsPhoneFill />
-                        <h3 className="ml-2">{users.mobile}</h3>
+                        <h3 className="ml-2">{user.mobile}</h3>
                       </div>
                       <div className="flex items-center justify-center">
                         <MdEmail />
-                        <h3 className="ml-2">{users.email}</h3>
+                        <h3 className="ml-2">{user.email}</h3>
                       </div>
                     </div>
 
@@ -99,58 +98,91 @@ const UserProfile = () => {
             </section>
           </main>
           :
-          <main className="profile-page flex justify-center items-center mt-60 ">
-            <section className="flex  ">
-              <div className="top-auto bottom-0 left-0 right-0 w-60 absolute pointer-events-none overflow-hidden h-70-px" style={{ transform: "translateZ(0px)" }}>
-                <svg className="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
-                  <polygon className="text-blueGray-200 fill-current" points="2560 0 2560 100 0 100"></polygon>
-                </svg>
-              </div>
-            </section>
-            <section className="relative py-10 bg-blueGray-200">
-              <div className="container mx-auto px-4">
-                <div className="relative card p-16 flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
-                  <form onSubmit={formik.handleSubmit} >
-                    <div className="px-6">
-                      <div className="flex flex-wrap justify-between">
+            formik.values && (
+              <div className="flex items-center h-screen w-full py-11 justify-center">
+                
+                <div className="max-w-xs">
+                  <div className="bg-white card shadow-xl rounded-lg w-fit p-20 ">
+                  <span onClick={() => setEditMode(false)} className="text-sm text-start font-bold block underline cursor-pointer tracking-wide text-blueGray-600">Back</span>
 
-                        <div className="w-full lg:w-4/12  px-4 lg:order-3 lg:text-right lg:self-center">
-                          <div className="py-6 px-3 sm:mt-0">
-                            <button className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" type="submit">
-                              Save
-                            </button>
-                          </div>
-                        </div>
-                        <div className="w-full lg:w-4/12 px-4 lg:order-1">
-                          <div className="flex justify-center py-4 lg:pt-4 pt-8">
-                            <div className="mr-4 p-3 text-center">
-                              <span onClick={() => setEditMode(false)} className="text-sm font-bold block underline cursor-pointer tracking-wide text-blueGray-600">Back</span>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className=" flex justify-center items-center align-middle">
-                        <img
-                          alt="..."
-                          src="https://source.unsplash.com/100x100/?portrait"
-                          className="shadow-xl rounded-full   border-none mr-16  ml-20 lg:ml-16 max-w-150-px"
-                        />
-                      </div>
-                      <div className="text-center mt-12">
-                        <input onChange={formik.handleChange} name='name' id='name' type='text' placeholder={users.name} className='input' />
-
-                        <div className="flex items-center justify-center mb-4 mt-5">
-                          <input onChange={formik.handleChange} name='mobile' id='mobile' type='number' className="ml-2 input" placeholder={users.mobile} />
-                        </div>
-                      </div>
-
+                    <div className="photo-wrapper p-2">
+                      <img
+                        className="w-32 h-32 rounded-full mx-auto"
+                        src="https://source.unsplash.com/100x100/?portrait"
+                        alt="John Doe"
+                      />
                     </div>
-                  </form>
+                    <div className="p-2">
+                      <h3 className="text-center text-xl text-gray-900 font-medium leading-8">
+                        {user.name}
+                      </h3>
+                      {/* <div className="text-center text-gray-400 text-xs font-semibold">
+                                <p>Web Developer</p>
+                            </div> */}
+                      <form onSubmit={formik.handleSubmit}>
+                        <table className="text-xs my-3">
+                          <tbody>
+                            {/* <tr>
+                                        <td className="px-2 py-2 text-gray-500 font-semibold">Address</td>
+                                        <td className="px-2 py-2">Chatakpur-3, Dhangadhi Kailali</td>
+                                    </tr> */}
+                            <tr>
+                              <td className="px-2 py-2 text-gray-500 font-semibold">
+                                Name
+                              </td>
+                              <td className="px-2 py-2">
+                                <input
+                                  className="input-xs"
+                                  onChange={formik.handleChange}
+                                  type="text"
+                                  name="name"
+                                  value={formik.values.name}
+                                />
+                                {formik.touched.name && formik.errors.name ? (
+                                  <div className="text-red-500">
+                                    {" "}
+                                    {formik.errors.name}{" "}
+                                  </div>
+                                ) : null}
+                              </td>
+                            </tr>
+                            <tr>
+                              <td className="px-2 py-2 text-gray-500 font-semibold">
+                                Mobile
+                              </td>
+                              <td className="px-2 py-2">
+                                <input
+                                  className="input-xs"
+                                  onChange={formik.handleChange}
+                                  type="number"
+                                  name="mobile"
+                                  value={formik.values.mobile}
+                                />
+                                {formik.touched.mobile && formik.errors.mobile ? (
+                                  <div className="text-red-500">
+                                    {" "}
+                                    {formik.errors.mobile}{" "}
+                                  </div>
+                                ) : null}
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                        <div className="text-center my-3">
+                          <button
+                            className="text-xs text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium"
+                            type="submit"
+                          >
+                            Save Profile
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
                 </div>
               </div>
-
-            </section>
-          </main>
+            )
+    
       }
     </>
   );
