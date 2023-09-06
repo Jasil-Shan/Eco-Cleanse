e3193c635eb87d86fd1f590a94d454b7cb48169f
Jasil Shan <jasilshan786@gmail.com>
2023-09-06 11:25:49 +0530

    route added

--- client/src/routes/AdminRouter.jsx
+++ client/src/routes/AdminRouter.jsx
@@ -7,6 +7,8 @@ import PrivateRoutes from "../utils/PrivateRoutes";
 import AdminWorkPage from "../pages/Admin/AdminWorkPage";
 import AdminDashboardPage from "../pages/Admin/AdminDashboardPage";
 import AdminDataTablePage from "../pages/Admin/AdminDataTablePage";
+import AdminPaymentPage from "../pages/Admin/AdminPaymentPage";
+import AdminGarbageReport from "../components/Admin/AdminGarbageReport";
 
 
 
@@ -20,6 +22,8 @@ function AdminRouter() {
                 <Route path="users" element={<AdminUsersPage />} />
                 <Route path="workers" element={<AdminWorkersPage />} />
                 <Route path="details" element={<AdminDataTablePage />} />
+                <Route path="payment" element={<AdminPaymentPage />} />
+                <Route path="garbageReport" element={<AdminGarbageReport />} />
             </Route>
             <Route path="login" element={<AdminLoginPage />} />
         </Routes>
