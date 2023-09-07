d33af4e687d0cd1bcceb8c63f802ac19ce5af352
Jasil Shan <jasilshan786@gmail.com>
2023-09-07 18:19:31 +0530

    bug fix

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5000", "http://ecocleanse.surge.sh","http://ecocleanse.surge.sh/admin/login" ]
+    origin: ["http://localhost:5000", "http://ecocleanse.surge.sh","https://ecocleanse.netlify.app" ]
   },
 });
 
@@ -68,7 +68,8 @@ app.use(
     cors({
       origin: [
         "http://localhost:5000",
-        "http://ecocleanse.surge.sh/"
+        "http://ecocleanse.surge.sh",
+       "https://ecocleanse.netlify.app"
       ],
       credentials: true,
     })
