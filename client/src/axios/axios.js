6651c3b44ebd4a0e43e3cf78cf7a1b248acbe9ee
Jasil Shan <jasilshan786@gmail.com>
2024-01-09 13:49:02 +0530

    bug fix

--- client/src/axios/axios.js
+++ client/src/axios/axios.js
@@ -2,8 +2,8 @@ import axios from 'axios' ;
 
 const axiosInstance = (tokenName)=> {
   const instance = axios.create({
-    // baseURL:'https://ecocleanse.comicworld.store' ,
-    baseURL:"http://localhost:3000" ,
+    baseURL:'https://ecocleanse.comicworld.store' ,
+    // baseURL:"http://localhost:3000" ,
     timeout : 15000 ,
     headers :{
       'Content-Type' : 'application/json'
@@ -12,7 +12,7 @@ const axiosInstance = (tokenName)=> {
   // instance request interceptor 
   instance.interceptors.request.use((request)=>{
     const token = localStorage.getItem(tokenName)
-    request.headers.Authorization = `Bearer ${token}` 
+    request.headers.Authorization = `Bearer ${token}`  
     return request 
   })
 
