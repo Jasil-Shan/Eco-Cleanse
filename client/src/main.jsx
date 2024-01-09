9df695af7db88023ccdf91ae87a8683e387144b3
Jasil Shan <jasilshan786@gmail.com>
2024-01-09 11:21:30 +0530

    Google auth provider added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,16 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from "react-redux"
 import store from './redux/store/store.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const clientId = "408273330574-55uafg4flb6fnmf6serr742svmamf6vq.apps.googleusercontent.com"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <GoogleOAuthProvider clientId={clientId}>
+        <App />
+      </GoogleOAuthProvider>
     </Provider>
   </React.StrictMode>,
 )
