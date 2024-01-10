2fea16a904eba015181e0c16bae8d8b780ee07a8
Jasil Shan <jasilshan786@gmail.com>
2024-01-10 20:41:03 +0530

    ui fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,11 @@
 import { Suspense, lazy, useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import AdminLoginPage from './pages/Admin/AdminLoginPage'
 import axios from 'axios'
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
-import UserRouter from './routes/UserRouter';
 import DriverRouter from './routes/DriverRouter';
 import WorkerRouter from './routes/WorkerRouter';
 import AdminRouter from './routes/AdminRouter';
-import { BeatLoader } from 'react-spinners';
 import Loader from './components/Loader/Loader';
 
 const LazyUserRoutes = lazy(() => import("./routes/UserRouter"));
