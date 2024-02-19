ed9422aa798291d947a40fc5fa7851b13ce99dd9
Jasil Shan <jasilshan786@gmail.com>
2024-02-20 00:59:41 +0530

    ui fix

--- client/src/components/User/Hero/HeroLeft.jsx
+++ client/src/components/User/Hero/HeroLeft.jsx
@@ -7,18 +7,14 @@ const HeroLeft = ({ user, recycle, last }) => {
     <div className="hero mt-28">
       <div className="hero-content drop-shadow-xl flex-col lg:flex-row">
         {user && <img src={user} loading="lazy" className="max-w-xs rounded-lg shadow-2xl" />}
-        {recycle && <img src={recycle} loading="lazy"  className="max-w-xs rounded-lg shadow-2xl" />}
+        {recycle && <img src={recycle} loading="lazy" className="max-w-xs rounded-lg shadow-2xl" />}
         <div className="drop-shadow-xl ml-4 rounded-lg bg-base-200 p-16">
-          {last ? <span><p className="text-2xl font-bold">We will Recycle it for a better Earth </p>  <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p> </span> :<span><p className="text-2xl font-bold">Just a Click and Update Details </p>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p> </span>}
+          {last ? <span><p className="text-2xl font-bold">We will Recycle it for a better Earth </p>  <p className="py-6 italic">
+          We are committed to recycling for a better Earth, striving to contribute to a healthier planet by repurposing your waste materials responsibly and sustainably
+          </p> </span> : <span><p className="text-2xl font-bold">Just a Click and Update Details </p>
+            <p className="py-6 italic">
+              Simply book a garbage collection from the comfort of your home with just a click! Pack your garbage and effortlessly update the details on our beautifully interactive cards on the next page.
+            </p> </span>}
           {/* <button className="btn btn-primary">Get Started</button> */}
         </div>
       </div>
